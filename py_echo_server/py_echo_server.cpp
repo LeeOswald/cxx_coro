bbef570f4b8e48043f30411d1addd258d715acb8
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2025-06-04 15:34:13 +0300

    refactoring

--- py_echo_server/py_echo_server.cpp
+++ py_echo_server/py_echo_server.cpp
@@ -14,8 +14,12 @@
 
 #include <boost/asio.hpp>
 
+
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#define Py_BUILD_CORE
+#include <internal/pycore_modsupport.h>
+
 
 extern "C" PyAPI_FUNC(int) _PyArg_ParseStackAndKeywords(
     PyObject *const *args,
@@ -23,6 +27,102 @@ extern "C" PyAPI_FUNC(int) _PyArg_ParseStackAndKeywords(
     PyObject *kwnames,
     struct _PyArg_Parser *,
     ...);
+
+
+struct PyResult 
+{
+    PyObject* obj;
+    char* out;
+    Py_ssize_t len;
+};
+
+struct PythonApp 
+{
+    PythonApp(PyObject* app)
+        : app_{ app }
+        , vecCall_{ PyVectorcall_Function(app) } 
+    {
+        VerboseBlock("PythonApp::PythonApp()");
+    };
+
+    template <typename CT>
+    auto run(const std::vector<char>& data, PyObject* prev, CT&& token) 
+    {
+        VerboseBlock("PythonApp::run()");
+
+        auto init = [this, &data, prev](auto handler) 
+        {
+            VerboseBlock("PythonApp::run::init()");
+
+            std::move(handler)(run_impl(data, prev));
+        };
+
+        return boost::asio::async_initiate<CT, void(PyResult)>(init, token);
+    }
+
+    template <typename CT> auto decref_pyobj(PyObject* obj, CT&& token) 
+    {
+        VerboseBlock("PythonApp::decref_pyobj()");
+
+        auto init = [this, obj](auto handler) 
+        {
+            VerboseBlock("PythonApp::decref_pyobj::init()");
+
+            decref_pyobj_impl(obj);
+            std::move(handler)();
+        };
+
+        return boost::asio::async_initiate<CT, void()>(init, token);
+    }
+
+private:
+    PyResult run_impl(const std::vector<char>& data, PyObject* prev) 
+    {
+        VerboseBlock("PythonApp::run_impl()");
+
+        auto state{ PyGILState_Ensure() };
+
+        Py_XDECREF(prev);
+        PyResult ret{};
+
+        auto pyBytes{ PyBytes_FromStringAndSize(data.data(), data.size()) };
+        if (!pyBytes)
+            return ret;
+
+        ret.obj = vecCall_ ? vecCall_(app_, &pyBytes, 1, NULL) : PyObject_CallOneArg(app_, pyBytes);
+        Py_DECREF(pyBytes);
+
+        if (!ret.obj) 
+        {
+            PyErr_Print();
+            PyErr_Clear();
+            PyGILState_Release(state);
+            return ret;
+        }
+
+        if (PyBytes_AsStringAndSize(ret.obj, &ret.out, &ret.len)) 
+        {
+            PyErr_Print();
+            PyErr_Clear();
+            Py_CLEAR(ret.obj);
+        }
+
+        PyGILState_Release(state);
+        return ret;
+    }
+
+    void decref_pyobj_impl(PyObject* obj) 
+    {
+        VerboseBlock("PythonApp::decref_pyobj_impl()");
+
+        auto state{ PyGILState_Ensure() };
+        Py_DECREF(obj);
+        PyGILState_Release(state);
+    }
+
+    PyObject* app_;
+    vectorcallfunc vecCall_;
+};
 
 namespace util
 {
@@ -38,16 +138,15 @@ constexpr auto nbeswap(std::integral auto val) noexcept
 } // namespace util {}
 
 
-boost::asio::awaitable<void> client_handler(boost::asio::ip::tcp::socket s, PyObject* app) 
+boost::asio::awaitable<void> client_handler(boost::asio::ip::tcp::socket s, PythonApp& app)
 {
     VerboseBlock("client_handler()");
 
-    PyObject* appReturn{ nullptr };
+    PyResult pr{};
 
     try 
     {
         std::vector<char> data;
-        auto vecCall{ PyVectorcall_Function(app) };
 
         for (;;) 
         {
@@ -62,66 +161,28 @@ boost::asio::awaitable<void> client_handler(boost::asio::ip::tcp::socket s, PyOb
             
             Info("received [{}]", cxx_coro::binaryToAscii({ data.data(), sz }));
 
-            char* out = nullptr;
-            Py_ssize_t len = 0;
-            {
-                VerboseBlock("GIL locked");
+            pr = co_await app.run(data, pr.obj, boost::asio::deferred);
+            if (!pr.obj)
+                break;
 
-                auto state{ PyGILState_Ensure() };
-                Py_XDECREF(appReturn);
+            Verbose("sending {} bytes...", pr.len);
 
-                auto pyBytes{ PyBytes_FromStringAndSize(data.data(), data.size()) };
-                if (!pyBytes)
-                {
-                    PyGILState_Release(state);
-                    break;
-                }
+            hdr = util::nbeswap(static_cast<std::uint16_t>(pr.len));
 
-                Verbose("Calling out...");
-                appReturn = vecCall ? vecCall(app, &pyBytes, 1, NULL) : PyObject_CallOneArg(app, pyBytes);
-
-                Py_DECREF(pyBytes);
-
-                if (!appReturn) 
-                {
-                    PyErr_Print();
-                    PyErr_Clear();
-                    PyGILState_Release(state);
-                    break;
-                }
-
-                
-                if (PyBytes_AsStringAndSize(appReturn, &out, &len)) 
-                {
-                    PyErr_Print();
-                    PyErr_Clear();
-                    Py_DECREF(appReturn);
-                    PyGILState_Release(state);
-                    break;
-                }
-
-                PyGILState_Release(state);
-            }
-
-            Verbose("sending {} bytes...", len);
-
-            hdr = util::nbeswap(static_cast<std::uint16_t>(len));
-
-            std::array seq{ boost::asio::buffer(&hdr, sizeof(hdr)), boost::asio::buffer(out, len) };
+            std::array seq{ boost::asio::buffer(&hdr, sizeof(hdr)), boost::asio::buffer(pr.out, pr.len) };
             co_await boost::asio::async_write(s, seq, boost::asio::deferred);
         }
     }
     catch (std::exception& e)
     {
         Error("Caught [{}]", e.what());
-
-        auto state{ PyGILState_Ensure() };
-        Py_XDECREF(appReturn);
-        PyGILState_Release(state);
     }
+
+    if (pr.obj)
+        co_await app.decref_pyobj(pr.obj, boost::asio::deferred);
 }
 
-boost::asio::awaitable<void> listener(boost::asio::ip::tcp::endpoint ep, PyObject* app) 
+boost::asio::awaitable<void> listener(boost::asio::ip::tcp::endpoint ep, PythonApp& app)
 {
     VerboseBlock("listener()");
 
@@ -138,7 +199,7 @@ boost::asio::awaitable<void> listener(boost::asio::ip::tcp::endpoint ep, PyObjec
     }
 }
 
-void accept(boost::asio::execution::executor auto ex, std::string_view host, std::string_view port, PyObject* app) 
+void accept(boost::asio::execution::executor auto ex, std::string_view host, std::string_view port, PythonApp& app)
 {
     VerboseBlock("accept()");
 
@@ -161,15 +222,15 @@ PyObject* run(PyObject* self, PyObject* const* args, Py_ssize_t nargs, PyObject*
     static const char* keywords[]{ "app", "host", "port" };
     static _PyArg_Parser parser{ .format = "O|ss:run", .keywords = keywords };
 
-    PyObject* app;
+    PyObject* appObj;
     const char* host = "localhost";
     const char* port = "8000";
 
 
-    if (!_PyArg_ParseStackAndKeywords(args, nargs, kwnames, &parser, &app, &host, &port))
+    if (!_PyArg_ParseStackAndKeywords(args, nargs, kwnames, &parser, &appObj, &host, &port))
         return nullptr;
 
-    Py_IncRef(app);
+    Py_IncRef(appObj);
 
     auto old_sigint{ std::signal(SIGINT, SIG_DFL) };
 
@@ -181,6 +242,7 @@ PyObject* run(PyObject* self, PyObject* const* args, Py_ssize_t nargs, PyObject*
         io.stop();
     });
 
+    PythonApp app{ appObj };
     accept(io.get_executor(), host, port, app);
 
     PyThreadState* thread{ PyEval_SaveThread() };
@@ -191,7 +253,7 @@ PyObject* run(PyObject* self, PyObject* const* args, Py_ssize_t nargs, PyObject*
     std::signal(SIGINT, old_sigint);
 
     PyEval_RestoreThread(thread);
-    Py_DecRef(app);
+    Py_DecRef(appObj);
     Py_RETURN_NONE;
 }
 
